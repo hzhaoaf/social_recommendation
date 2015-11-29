ef3b18d76a9652b80b7cc0ef6867a63d9e6a3694
ZhaoHuan <byzhaohuan@gmail.com>
2015-11-29 20:18:22 +0800

    normalized data: minus mean for all raw ratings

--- pmf.py
+++ pmf.py
@@ -33,7 +33,7 @@ sigmod_d = np.vectorize(lambda x: sigmod_der(x))
 
 class PMF(object):
 
-    def __init__(self, use_normalized_data=False, start_from_zero=False, learning_rate=0.001):
+    def __init__(self, use_normalized_data=True, start_from_zero=False, learning_rate=0.001):
         init_logger(log_file='log/pmf.log', log_level=logging.INFO)
         self.exp_id = datetime.today().strftime('%Y%m%d%H%M%S')
         self.ratings_file = ratings_file
@@ -51,7 +51,7 @@ class PMF(object):
         self.lamb = 0.01 #Regularization parameter
         self.momentum = 0.8
         self.max_epoch = 1000 #iteration
-        self.feat_num = 5
+        self.feat_num = 10
 
         #uid, vid以observation里出现的uid为准, 如何划分数据也是一个问题
         self.user_num = self.ratings_vector[:,0].max()
@@ -87,12 +87,18 @@ class PMF(object):
         self.ratings_vector = np.loadtxt(self.ratings_file,delimiter=' ')
         self.ratings_vector = self.ratings_vector[:,[0,1,2]]
 
-    def generate_normalized_ratings(self):
+    def generate_normalized_ratings(self, minus_mean=True, mapping_01=False):
         '''
             mapping the rating 1,...,K to [0, 1] by the formula r = (x - 1) / (K - 1)
         '''
-        max_ = self.ratings_vector[:,2].max()
-        self.ratings_vector[:,2] = (self.ratings_vector[:,2] - 1.0) / (max_ - 1)
+        self.mapping_01 = mapping_01
+        self.minus_mean = minus_mean
+        if self.mapping_01:
+            max_ = self.ratings_vector[:,2].max()
+            self.ratings_vector[:,2] = (self.ratings_vector[:,2] - 1.0) / (max_ - 1)
+        if self.minus_mean:
+            mean = np.mean(self.ratings_vector[:,2])
+            self.ratings_vector[:,2] -= mean
 
     def train(self):
         '''
@@ -117,7 +123,7 @@ class PMF(object):
                 U_V_pairwise = np.multiply(self.U[user_inds,:], self.V[item_inds,:])
 
                 #####compute predictions####
-                if self.use_normalized_data:
+                if self.use_normalized_data and self.mapping_01:
                     pred_out = sigmod_f(U_V_pairwise.sum(axis=1))#|R| * K --> |R| * 1
                 else:
                     pred_out = U_V_pairwise.sum(axis=1)#|R| * K --> |R| * 1
@@ -130,7 +136,7 @@ class PMF(object):
                 grad_u = np.zeros(self.U_shape)
                 grad_v = np.zeros(self.V_shape)
 
-                if self.use_normalized_data:
+                if self.use_normalized_data and self.mapping_01:
                     ####update gradient
                     sigmod_dot = sigmod_f(U_V_pairwise.sum(axis=1))
                     sigmod_der_V = sigmod_d(U_V_pairwise.sum(axis=1))
@@ -138,7 +144,6 @@ class PMF(object):
                     delta_matrix = np.tile(U_V_delta, self.feat_num) # |R| * K, 这样就可以使得u_i和对应的v_j进行dot product, 可以直接使用矩阵运算
                     delta_U = np.multiply(delta_matrix, self.V[item_inds,:])
                     delta_V = np.multiply(delta_matrix, self.U[user_inds,:])
-                    dot_time = time.time()
 
                 else:
                     ####update gradient
@@ -146,7 +151,7 @@ class PMF(object):
                     delta_matrix = np.tile(U_V_delta, self.feat_num) # |R| * K, 这样就可以使得u_i和对应的v_j进行dot product, 可以直接使用矩阵运算
                     delta_U = np.multiply(delta_matrix, self.V[item_inds,:])
                     delta_V = np.multiply(delta_matrix, self.U[user_inds,:])
-                    dot_time = time.time()
+                dot_time = time.time()
 
                 ind = 0
                 for uid, vid, r in self.train_vector:
@@ -159,7 +164,7 @@ class PMF(object):
 
                 accumulate_time = time.time()
 
-                logging.info('dot/accumulate cost %.1fs/%.1fs', dot_time - pred_time, accumulate_time - dot_time)
+                logging.debug('dot/accumulate cost %.1fs/%.1fs', dot_time - pred_time, accumulate_time - dot_time)
 
                 grad_u += self.lamb * self.U
                 grad_v += self.lamb * self.V
@@ -186,7 +191,7 @@ class PMF(object):
         logging.info('training finished, cost %.2fmin', (time.time() - train_start) / 60.0)
         self.predict()
         self.evaluate()
-        logging.info('config: iters=%s, feat=%s, regularization=%s, learning_rate=%s, normalized_data=%s', real_iter, self.feat_num, self.lamb, self.epsilon, self.use_normalized_data)
+        logging.info('config: iters=%s, feat=%s, regularization=%s, learning_rate=%s, normalized_data=%s', real_iter, self.feat_num, self.lamb, self.epsilon, 'minus_mean' if self.minus_mean else '')
         logging.info('****************Experiment@%s*******************', self.exp_id)
 
     def predict(self):
@@ -200,7 +205,7 @@ class PMF(object):
             user_inds = self.vali_vector[:,0].astype(int) - 1
             item_inds = self.vali_vector[:,1].astype(int) - 1
 
-        if self.use_normalized_data:
+        if self.use_normalized_data and self.mapping_01:
             self.predictions = sigmod_f(np.multiply(self.U[user_inds,:], self.V[item_inds,:]).sum(axis=1))
         else:
             self.predictions = np.multiply(self.U[user_inds,:], self.V[item_inds,:]).sum(axis=1)
@@ -212,11 +217,11 @@ class PMF(object):
         rand_inds = np.random.permutation(len(self.predictions))
         sample_ids = rand_inds[:100]
         vali_ratings = self.vali_vector[:,2]
-        samples = [(round(self.predictions[r],1), vali_ratings[r]) for r in sample_ids]
+        samples = [(round(self.predictions[r],1), round(vali_ratings[r], 1)) for r in sample_ids]
         logging.info('prediction samples: %s', samples)
         #大于5小于1改成1，5
-        self.predictions[self.predictions > 5.0] = 5.0
-        self.predictions[self.predictions < 1.0] = 1.0
+        #self.predictions[self.predictions > 5.0] = 5.0
+        #self.predictions[self.predictions < 1.0] = 1.0
         delta = self.predictions - vali_ratings
         mae = np.absolute(delta).sum() / delta.shape[0]
         rmse = math.sqrt(np.square(delta).sum() / delta.shape[0])
@@ -224,7 +229,7 @@ class PMF(object):
 
     def run(self):
         logging.info('****************Experiment@%s*******************', self.exp_id)
-        logging.info('config: iters=%s, feat=%s, regularization=%s, learning_rate=%s, normalized_data=%s', self.max_epoch, self.feat_num, self.lamb, self.epsilon, self.use_normalized_data)
+        logging.info('config: iters=%s, feat=%s, regularization=%s, learning_rate=%s, normalized_data=%s', self.max_epoch, self.feat_num, self.lamb, self.epsilon, 'minus_mean' if self.minus_mean else '')
         self.train()
 
 if __name__ == '__main__':
